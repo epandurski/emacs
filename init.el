12d74bb62cdc50bf35b08fff211ee732abeca839
Evgeni Pandurski <epandurski@gmail.com>
2022-07-14 14:06:30 +0300

    Do not set current-language-environment

--- init.el
+++ init.el
@@ -12,9 +12,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(cua-mode nil nil (cua-base))
  '(cua-remap-control-v nil)
- '(current-language-environment "UTF-8")
  '(default-input-method "bulgarian-phonetic")
  '(dired-listing-switches "-al --time-style=long-iso --group-directories-first")
  '(dired-omit-files "^\\.?#\\|^\\.$\\|^\\.\\.$\\|^\\..+$")

212dcb55a9f0c86787173ff5703c2ce4f818a407
Evgeni Pandurski <epandurski@gmail.com>
2022-12-08 22:12:16 +0200

    Bind C-z back, it seems I miss it

--- my-base-bindings.el
+++ my-base-bindings.el
@@ -84,8 +84,8 @@
   (global-unset-key (kbd key)))
 
 ;; Use "C-z" when not on text terminal:
-;; (when window-system
-;;   (global-set-key (kbd "C-z") 'undo))
+(when window-system
+  (global-set-key (kbd "C-z") 'undo))
 
 (defun my-minibuffer-keys ()
   "My keybindings for the minibuffer."

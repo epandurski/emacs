4b354c01b2f1d6700fbf25f94a464bf7300f96a5
Evgeni Pandurski <epandurski@gmail.com>
2023-01-08 21:19:50 +0200

    Improve my-toggle-letter-case

--- my-util-funcs.el
+++ my-util-funcs.el
@@ -45,43 +45,51 @@ With ARG, insert ARG lines."
     (exit-recursive-edit))
   (command-execute 'bookmark-bmenu-list))
 
+(defun my--get-word-boundaries ()
+  "Return the buffer location of the current word or text selection.
+If there are no region selected, return the location of the
+current word. Returns a zero-length interval, if a word can not
+be found near the point. The returned value is a cons cell."
+  (if (region-active-p)
+      (cons (region-beginning) (region-end))
+    (or (bounds-of-thing-at-point 'word)
+        (cons (point) (point)))))
+
 (defun my-upcase-letter-case ()
   "Downcase the current word or text selection."
   (interactive)
   (let (p1 p2 (deactivate-mark nil))
-    (if (region-active-p)
-	(setq p1 (region-beginning) p2 (region-end))
-      (let ((bds (bounds-of-thing-at-point 'symbol) ) )
-	(setq p1 (car bds) p2 (cdr bds)) ) )
+    (pcase-setq `(,p1 . ,p2) (my--get-word-boundaries))
     (upcase-region p1 p2)))
 
 (defun my-toggle-letter-case ()
   "Toggle the letter case of current word or text selection.
 
-Toggles between: “all lower”, “Init Caps”, “ALL CAPS”."
+Toggles between: 'all lower', 'Init Caps', 'ALL CAPS'."
   (interactive)
-  (let (p1 p2 (deactivate-mark nil) (case-fold-search nil))
-    (if (region-active-p)
-	(setq p1 (region-beginning) p2 (region-end))
-      (let ((bds (bounds-of-thing-at-point 'symbol) ) )
-	(setq p1 (car bds) p2 (cdr bds)) ) )
-    (when (not (eq last-command this-command))
+  (let (p1 p2 state (deactivate-mark nil) (case-fold-search nil))
+    (if (eq last-command this-command)
+        (progn
+          (setq p1 (get this-command 'p1))
+	  (setq p2 (get this-command 'p2))
+          (setq state (get this-command 'state)))
+      (pcase-setq `(,p1 . ,p2) (my--get-word-boundaries))
       (save-excursion
 	(goto-char p1)
-	(cond
-	 ((looking-at "[[:lower:]][[:lower:]]") (put this-command 'state "all lower"))
-	 ((looking-at "[[:upper:]][[:upper:]]") (put this-command 'state "all caps"))
-	 ((looking-at "[[:upper:]][[:lower:]]") (put this-command 'state "init caps"))
-	 ((looking-at "[[:lower:]]") (put this-command 'state "all lower"))
-	 ((looking-at "[[:upper:]]") (put this-command 'state "all caps"))
-	 (t (put this-command 'state "all lower")))))
-    (cond
-     ((string= "all lower" (get this-command 'state))
-      (upcase-initials-region p1 p2) (put this-command 'state "init caps"))
-     ((string= "init caps" (get this-command 'state))
-      (upcase-region p1 p2) (put this-command 'state "all caps"))
-     ((string= "all caps" (get this-command 'state))
-      (downcase-region p1 p2) (put this-command 'state "all lower")))))
+        (setq state (cond
+	       ((looking-at "[^[:word:]]*[[:upper:]][[:upper:]]") "all caps")
+	       ((looking-at "[^[:word:]]*[[:upper:]][[:lower:]]") "init caps")
+	       ((looking-at "[^[:word:]]*[[:upper:]]") "all caps")
+               ("all lower"))))
+      (put this-command 'p1 p1)
+      (put this-command 'p2 p2))
+    (put this-command 'state (cond
+          ((string= "all lower" state)
+           (upcase-initials-region p1 p2) "init caps")
+          ((string= "init caps" state)
+           (upcase-region p1 p2) "all caps")
+          ((string= "all caps" state)
+           (downcase-region p1 p2) "all lower")))))
 
 (defun my-mark-current-symbol (&optional arg allow-extend)
   "Put point at beginning of current symbol, set mark at end."

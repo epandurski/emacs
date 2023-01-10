025d78b4c95c2c46ca8c05ff99c8b3c53681ad7a
Evgeni Pandurski <epandurski@gmail.com>
2023-01-10 03:10:03 +0200

    Change hl-line, line-number-current-line faces

--- custom.el
+++ custom.el
@@ -77,7 +77,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
- '(highlight ((t (:background "lavender"))))
+ '(hl-line ((t (:inherit highlight :extend t :background "gray93"))))
+ '(line-number-current-line ((t (:inherit line-number :background "gray84"))))
  '(mode-line ((((class color) (min-colors 88)) (:background "#9dbde4" :foreground "black" :box (:line-width -1 :style released-button)))))
  '(region ((t (:background "LightGoldenrod2")))))
 
